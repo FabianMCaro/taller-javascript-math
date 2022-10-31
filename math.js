8baa765b752e34d14373f4722e05660599e2348b
Fabian Caro <fabiancaro101@gmail.com>
2022-10-31 13:18:58 -0500

    Calculo del area y perimetro de un Circulo y uso de la funcion Math

--- math.js
+++ math.js
@@ -1,3 +1,4 @@
+// Calculo del perimetro y del area de un cuadrado
 const ladoCuadrado = 5;
 const perimetroCuadrado = ladoCuadrado * 4;
 
@@ -18,6 +19,8 @@ function calcularCuadrado(lado) {
     }
 }
 
+
+// Calculo de Perimetro y area de un triangulo
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const ladoTrianguloBase = 4
@@ -43,3 +46,32 @@ console.log({
     areaTriangulo,
 });
 console.groupEnd('Triangulo')
+
+
+// Calculo de Perimetro y Area de una circulo
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+function calcularCirculo(radio){
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio,2);
+
+    return {
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI,
+    }
+}
+
+console.group('Circulo');
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circunferencia,
+    areaCirculo,
+});
+console.groupEnd('Circulo');
