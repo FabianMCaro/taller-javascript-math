583a65feb95afd45366d24eaf273344adafbcf02
Fabian Caro <fabiancaro101@gmail.com>
2022-11-01 15:38:33 -0500

    Ejemplo de lista de cupones con if anidados y Swtch case

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,5 @@
 const inputPrecio = document.querySelector('#precio');
-const inputDescuento = document.querySelector('#descuento');
+const inputCupon = document.querySelector('#cupon');
 const btn = document.querySelector('#calcular');
 const pResultado = document.querySelector('#resultado');
 
@@ -9,17 +9,37 @@ btn.addEventListener('click', calcularDescuento);
 
 function calcularDescuento(){
     const precio = Number(inputPrecio.value);
-    const descuento = Number(inputDescuento.value);
+    const cupon = inputCupon.value;
+    let descuento;
 
-    if (!precio || !descuento) {
+    if (!precio || !cupon) {
         pResultado.innerText = 'Por favor llene el formulario';
         return;
     }
-
-    if (descuento > 100){
-        pResultado.innerText = 'Aja, ya quisieras, no te vamos a dar plata, PAGA!!';
-        return;
+    
+    switch (cupon){
+        case 'cupon1':
+            descuento = 30;
+            break;
+        case 'cupon2':
+            descuento = 25;
+            break;
+        default:
+            pResultado.innerText = 'El cupon no es valido';
+            return;
     }
+
+
+    // if(cupon == 'aaaaa444'){
+    //     descuento = 30;
+    // }
+    // else if (cupon == 'aaaaa333'){
+    //     descuento = 25;
+    // }
+    // else{
+    //     pResultado.innerText = 'El cupon no es valido';
+    //     return;
+    // }
 
     // (p * (100 - d)) / 100
     const nuevoPrecio = (precio * (100 - descuento)) / 100;
