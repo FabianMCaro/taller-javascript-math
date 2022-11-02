fd744db8add285a950d732026b0bcd13f1630545
Fabian Caro <fabiancaro101@gmail.com>
2022-11-02 17:29:27 -0500

    Calcular la mediana de una lista par e impar

--- platzimath.js
+++ platzimath.js
@@ -11,18 +11,35 @@ function calcularPromedio(lista){
     const sumaLista = lista.reduce(sumarTodosElementos);
 
     let promedio = sumaLista / lista.length;
-    console.log(promedio);
+    //console.log(promedio);
     return promedio;
 }
 
 
 function esParOImpar (lista){
-
-    const resultado = lista.length % 2;
-    if(resultado == 0){ 
+    if(lista.length % 2 == 0){ 
         return true;
     }
     else {
         return false;
+    }
+}
+
+function calcularMediana (lista){
+    const listaEsPar = esParOImpar(lista);
+    if(listaEsPar){
+        const mitadListaPar1 = lista[(lista.length / 2) -1];
+        const mitadListaPar2 = lista[(lista.length / 2)];
+        const listaMitades = [mitadListaPar1, mitadListaPar2];
+        const medianaListaPar = calcularPromedio(listaMitades);
+        return medianaListaPar;
+    }
+    else{
+        // redondear y quitar decimales Math.floor 
+        const indexMitadListaImpar = Math.floor(lista.length / 2);
+        const medianaListaImpar = lista[indexMitadListaImpar];
+        console.log(indexMitadListaImpar);
+        console.log(medianaListaImpar);
+        return lista[indexMitadListaImpar];
     }
 }
