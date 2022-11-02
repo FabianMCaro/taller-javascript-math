0d27ce568bf38b23b65c3ac0347c5d6ca9dff499
Fabian Caro <fabiancaro101@gmail.com>
2022-11-02 13:19:03 -0500

    Metodo reduce de los arrays, para calcular el promedio de una lista

--- platzimath.js
+++ platzimath.js
@@ -1,8 +1,14 @@
 function calcularPromedio(lista){
-    let sumaLista = 0;
-    for(let i=0; i<lista.length; i++){
-        sumaLista = sumaLista + lista[i];
+    // let sumaLista = 0;
+    // for(let i=0; i<lista.length; i++){
+    //     sumaLista = sumaLista + lista[i];
+    // }
+
+    function sumarTodosElementos(valorAcumulado, nuevoValor) {
+        return valorAcumulado + nuevoValor;
     }
+
+    const sumaLista = lista.reduce(sumarTodosElementos);
 
     let promedio = sumaLista / lista.length;
     console.log(promedio);
