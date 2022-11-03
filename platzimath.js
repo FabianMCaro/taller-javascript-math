b4c6ab7850099596e8fe6712fb3c39a095794bc8
Fabian Caro <fabiancaro101@gmail.com>
2022-11-02 22:17:46 -0500

    Calcular la moda, guardar en array los elementos repetidos y ordenarlos

--- platzimath.js
+++ platzimath.js
@@ -57,9 +57,42 @@ function ordenarLista (listaDesordenada){
             return -1;
         }
         */
-       return valorAcumulado -nuevoValor
+       return valorAcumulado - nuevoValor;
     }
 
     const lista = listaDesordenada.sort(ordenarListaSort);
     return lista;
 }
+
+function CalcularModa (lista){
+    const listaCount = {};
+
+    for (let i=0; i<lista.length; i++){
+        const elemento = lista[i];
+
+        if(listaCount[elemento]){
+            listaCount[elemento] += 1;
+        }
+        else{
+            listaCount[elemento] = 1;
+        }   
+    }
+
+    const listaArray = Object.entries(listaCount);
+    const listaordenada = ordenarListaBidimensional (listaArray, 1);
+    const listaMaxNumber = listaordenada[listaordenada.length-1];
+    const moda = listaMaxNumber[0];
+    //console.log({listaCount, listaArray, listaordenada, listaMaxNumber});
+    //console.log('La moda es: ' + listaMaxNumber[0]);
+    return moda;
+}
+
+function ordenarListaBidimensional (listaDesordenada, i){
+    function ordenarListaSort(valorAcumulado, nuevoValor){
+       return valorAcumulado[i] - nuevoValor[i];
+    }
+
+    const lista = listaDesordenada.sort(ordenarListaSort);
+    return lista;
+}
+
