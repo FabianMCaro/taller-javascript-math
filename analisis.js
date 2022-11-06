d289c1ef349d61d09e6d778bc758869a158d2ab1
Fabian Caro <fabiancaro101@gmail.com>
2022-11-05 21:51:14 -0500

    proyeccion salarial por persona

--- analisis.js
+++ analisis.js
@@ -24,3 +24,27 @@ function medianaPorPersona(nombrePersona){
     console.log(salarios);
     return medianaSlarios;
 }
+
+function proyeccionPorPersona(nombrePersona) {
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+    
+    let porcentajesCrecimiento = [];
+
+    for (let i=1; i<trabajos.length; i++){
+        const salarioActual = trabajos[i].salario;
+        const salarioPasado = trabajos[i-1].salario;
+        const crecimiento = salarioActual - salarioPasado;
+        const porcentajeCrecimiento = crecimiento / salarioPasado;
+        porcentajesCrecimiento.push(porcentajeCrecimiento);
+    }
+    
+    const medianaProcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+
+    //console.log({porcentajesCrecimiento, medianaProcentajesCrecimiento});
+
+    const ultimoSalario = trabajos[trabajos.length-1].salario;
+    const aumento =  ultimoSalario * medianaProcentajesCrecimiento;
+    const nuevoSalario = ultimoSalario + aumento;
+    //console.log({nuevoSalario});
+    return nuevoSalario;
+}
